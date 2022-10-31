6d36596ed339b1166bef415f0245225a11f3d15d
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2022-10-31 09:15:33 +0330

    export default getHtml;

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const getHtml = async (url, tag, output) => require('fs').writeFileSync(output, require('cheerio').load((await require('axios').get(url)).data)(tag).html());
 
-module.exports = { getHtml };
+export default getHtml;
